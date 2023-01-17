3ae6a71a4615073045897d4f47e38ed3f14172f1
ehghksvjscl <ehghkswodnjs@naver.com>
2023-01-17 13:46:22 +0900

    feat: basic view setting for order

--- store/views/order.py
+++ store/views/order.py
@@ -1,3 +1,19 @@
 """
 Order View
 """
+
+from rest_framework import viewsets, mixins, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from store.models import Order
+from store.serializers import OrderSerializer
+
+class OrderViewSet(mixins.CreateModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
+
+    serializer_class = OrderSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Order.objects.all()
